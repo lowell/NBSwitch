890558fb11ee9854c1976c43d98cc5489639caef
[lowellv@me.com] <lowellv@me.com>
2014-07-17 21:28:19 -0700

    Consistency fixes:
    - pointer asterisks
    - misc spacing

--- NSObject+NBSwitch.m
+++ NSObject+NBSwitch.m
@@ -10,7 +10,7 @@
 #import <objc/runtime.h>
 
 // Implementation function declaration
-static void nb_switch(NBSwitch * self, SEL _cmd, ...);
+static void nb_switch(NBSwitch *self, SEL _cmd, ...);
 
 // Selector name templates
 #define CASE__   "case::"
@@ -32,47 +32,50 @@ static void nb_switch(NBSwitch * self, SEL _cmd, ...);
 
 + (BOOL)resolveInstanceMethod:(SEL)aSEL
 {
-    const char* selector = sel_getName(aSEL);
+    const char *selector = sel_getName(aSEL);
 
     // Check selector has a valid length,
     // count the probable number of args and
     // find whether there's a default block.
     BOOL hasDefaultBlock = (strlen(selector) % strlen(CASE__)) != 0;
-    if(hasDefaultBlock &&
-       strlen(selector)%strlen(CASE__) != strlen(DEFAULT_)-strlen(CASE__) )
+    if(hasDefaultBlock && strlen(selector)%strlen(CASE__) != strlen(DEFAULT_)-strlen(CASE__) )
         return NO;
-    size_t caseCount = (strlen(selector)-(hasDefaultBlock?strlen(DEFAULT_):0)) / strlen(CASE__);
+    size_t caseCount = (strlen(selector) - (hasDefaultBlock ? strlen(DEFAULT_) : 0)) / strlen(CASE__);
     
     // Check selector looks like "case::case::"...
-    for(size_t i=0;i<caseCount;i++)
-        if(memcmp(selector + i*strlen(CASE__), CASE__, strlen(CASE__)))
+    for(size_t i = 0; i < caseCount; i++)
+        if(memcmp(selector + i * strlen(CASE__), CASE__, strlen(CASE__)))
             return NO;
 
     // Check it ends with "default:"
-    if(hasDefaultBlock &&
-       memcmp(selector + caseCount*strlen(CASE__), DEFAULT_, strlen(DEFAULT_)))
+    if(hasDefaultBlock && memcmp(selector + caseCount * strlen(CASE__), DEFAULT_, strlen(DEFAULT_)))
             return NO;
     
     // Selector string is what we expect. Construct types encoding string.
     //
     // types looks like : v@:@@?@@?@@?@
     // where :
-    //  - v is for the (void) return
-    //  - @: are for the self and _cmd hidden params
+    //  - v   is for the (void) return
+    //  - @:  are for the self and _cmd hidden params
     //  - @@: is for each case
-    //   - @ for the object
-    //   - @? for the block
-    //  - @? is for the last ("default") block
-    NSMutableString * types = [NSMutableString stringWithFormat:@"%s%s%s",
-                               @encode(void),@encode(id),@encode(SEL)];
+    //  - @   for the object
+    //  - @?  for the block
+    //  - @?  is for the last ("default") block
+    NSMutableString *types = [NSMutableString stringWithFormat:@"%s%s%s",
+                                                               @encode(void),
+                                                               @encode(id),
+                                                               @encode(SEL)];
 
-    for(size_t i=0;i<caseCount;i++)
-        [types appendFormat:@"%s%s",@encode(id),@encode(void(^)(void))];
+    for(size_t i = 0; i < caseCount; i++)
+        [types appendFormat:@"%s%s",
+                            @encode(id),
+                            @encode(void(^)(void))];
 
     if(hasDefaultBlock)
-        [types appendFormat:@"%s",@encode(void(^)(void))];
+        [types appendFormat:@"%s",
+                            @encode(void(^)(void))];
 
-    class_addMethod(self, aSEL, (IMP) nb_switch, [types cStringUsingEncoding:NSASCIIStringEncoding]);
+    class_addMethod(self, aSEL, (IMP)nb_switch, [types cStringUsingEncoding:NSUTF8StringEncoding]);
     return YES;
 }
 
@@ -85,20 +88,20 @@ static void nb_switch(NBSwitch * self, SEL _cmd, ...);
 // Although there's no variadic in the public interface, we use one for the implementation.
 // We use the length of the selector to count the number of cases
 // and find if there's a "default" block.
-static void nb_switch(NBSwitch * self, SEL _cmd, ...)
+static void nb_switch(NBSwitch *self, SEL _cmd, ...)
 {
 	va_list args;
 	va_start(args, _cmd);
 
     // Find out number of arguments in our va_list
-    const char* selector = sel_getName(_cmd);
+    const char *selector = sel_getName(_cmd);
     BOOL hasDefaultBlock = (strlen(selector) % strlen(CASE__)) != 0;
-    size_t caseCount = (strlen(selector)-(hasDefaultBlock?strlen(DEFAULT_):0)) / strlen(CASE__);
-    assert( (strlen(selector)-(hasDefaultBlock?strlen(DEFAULT_):0)) / caseCount == strlen(CASE__) );
+    size_t caseCount = (strlen(selector) - (hasDefaultBlock ? strlen(DEFAULT_) : 0)) / strlen(CASE__);
+    assert( (strlen(selector) - (hasDefaultBlock ? strlen(DEFAULT_) : 0)) / caseCount == strlen(CASE__) );
 
     // loop for each "case"
     // (object,block) pair of arguments
-    for (size_t i=0; i<caseCount; i++)
+    for (size_t i = 0; i < caseCount; i++)
     {
         id value = va_arg(args, id);
         void (^block)(void) = va_arg(args, void (^)(void));
@@ -117,8 +120,8 @@ static void nb_switch(NBSwitch * self, SEL _cmd, ...)
         block();
     }
     
-cleanup:
-	va_end(args); 
+    cleanup:
+        va_end(args); 
 }
 
 // Our NSObject category to create the NBSwitch, simply used as a trampoline object.
@@ -129,8 +132,8 @@ cleanup:
 @implementation NSObject (NBSwitch)
 - (NBSwitch *) switch
 {
-    NBSwitch * sw_ = [NBSwitch new];
-    sw_->receiver = self;
-    return sw_;
+    NBSwitch *s = [NBSwitch new];
+    s->receiver = self;
+    return s;
 }
 @end
